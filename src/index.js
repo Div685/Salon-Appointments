c757a9b848fc88127ca1ceefa968cc40557723ae
Div685 <divyeshpatel685@gmail.com>
2021-05-14 11:23:26 +0300

    clearn up project
    - removed some files to restructure the project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Welcome to Salon</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
