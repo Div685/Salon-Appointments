520a2854988c4727e9dee99503c93e88152a2987
Div685 <divyeshpatel685@gmail.com>
2021-05-15 22:58:21 +0300

    Add UserForm component to access the Form.
    - contails onSubmit method to submit the data

---
+++ src/components/UserForm.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const UserForm = ({ handleSubmit, btnName }) => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    handleSubmit(userName, password);
+  };
+
+  return (
+    <div className="form_section">
+      <form className="form" onSubmit={onSubmitForm}>
+        <input
+          className="form_input"
+          placeholder="Username"
+          type="text"
+          name="username"
+          value={userName}
+          onChange={(event) => { setUserName(event.target.value); }}
+        />
+        <input
+          className="form_input"
+          placeholder="password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={(event) => { setPassword(event.target.value); }}
+        />
+
+        <button type="submit" className="btn ">{btnName}</button>
+      </form>
+    </div>
+  );
+};
+
+UserForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  btnName: PropTypes.string.isRequired,
+};
+
+export default UserForm;
