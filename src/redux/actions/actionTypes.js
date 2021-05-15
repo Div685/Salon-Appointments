98555ba07efa922104914129a0701748e971ef2a
Div685 <divyeshpatel685@gmail.com>
2021-05-15 13:48:06 +0300

    Add ActionTypes and actions to the action directory

---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,13 @@
+// Users
+export const LOG_IN = 'LOG_IN';
+export const SIGN_UP = 'SIGN_UP';
+
+// Items
+export const ADD_ITEMS = 'ADD_ITEMS';
+
+// Appointment
+export const ADD_APPOINTMENTS = 'ADD_APPOINTMENTS';
+
+// Appointment date
+export const ADD_APPOINTMENT_DATES = 'ADD_APPOINTMENT_DATES';
+export const REMOVE_APPOINTMENT_DATES = 'REMOVE_APPOINTMENT_DATES';
---
+++ src/redux/actions/index.js
@@ -0,0 +1,37 @@
+import {
+  LOG_IN,
+  SIGN_UP,
+  ADD_ITEMS,
+  ADD_APPOINTMENTS,
+  ADD_APPOINTMENT_DATES,
+  REMOVE_APPOINTMENT_DATES,
+} from './actionTypes';
+
+export const logIn = (logIn = true) => ({
+  type: LOG_IN,
+  logIn,
+});
+
+export const signUp = (user = {}) => ({
+  type: SIGN_UP,
+  user,
+});
+
+export const addItems = (items = []) => ({
+  type: ADD_ITEMS,
+  items,
+});
+
+export const addAppointments = (appointments = []) => ({
+  type: ADD_APPOINTMENTS,
+  appointments,
+});
+
+export const addAppointmentDates = (appointmentDates = []) => ({
+  type: ADD_APPOINTMENT_DATES,
+  appointmentDates,
+});
+
+export const removeAppointmentDates = () => ({
+  type: REMOVE_APPOINTMENT_DATES,
+});
