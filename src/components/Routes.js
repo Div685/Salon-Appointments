923f2043ef41d4dc1926342d475c79091ec85f19
Div685 <divyeshpatel685@gmail.com>
2021-05-15 22:56:35 +0300

    Add Signup Routes and Component

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // import SalonItemList from '../container/SalonItemList';
 import Login from '../container/Login';
+import SignUp from '../container/SignUp';
 import Home from './Home';
 
 const Routes = () => (
@@ -9,6 +10,7 @@ const Routes = () => (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={SignUp} />
     </Switch>
   </BrowserRouter>
 );
---
+++ src/container/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import UserForm from '../components/UserForm';
+
+const SignUp = () => (
+  <div>
+    <h2>Sign up</h2>
+    <UserForm />
+  </div>
+);
+
+export default SignUp;
