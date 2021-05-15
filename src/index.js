9d2d5faec362f2d800a729710386daa0650fddce
Div685 <divyeshpatel685@gmail.com>
2021-05-15 23:10:52 +0300

    Add bootstrap and imported in index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+/* eslint-disable no-unused-vars */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
