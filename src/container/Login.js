7b6d9b96759c2828bf6e443ec8042e1229e26fa5
Div685 <divyeshpatel685@gmail.com>
2021-05-16 23:53:24 +0300

    Add Login func to the component
    - get loginform from userForm component
    - Added useState message to display message or error
    - store data to the redux using mapState, dispatch,and connect.
    - check if user is logged in then redirect to home page using loginUser prop

--- src/container/Login.js
+++ src/container/Login.js
@@ -1,17 +1,66 @@
-import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { loggedIn } from '../api/authUserRequest';
 import UserForm from '../components/UserForm';
+import { logIn, signUp } from '../redux/actions';
 
-const Login = () => {
-  const handleSubmit = () => {
+const Login = ({
+  logIn, signUp, history, loginUser,
+}) => {
+  const [message, setMessage] = useState([]);
 
+  const handleLogin = async (userName, password) => {
+    try {
+      const response = await loggedIn(userName, password);
+      if (response.logged_in) {
+        setMessage(['SuccessFully Logged in!']);
+        localStorage.setItem('token', response.token);
+        signUp(response.user);
+        logIn(true);
+        history.push('/items');
+      } else {
+        setMessage(response.message);
+      }
+    } catch {
+      setMessage(['Sorry, Logged in failed ']);
+    }
   };
 
-  return (
+  const handleSubmit = (userName, password) => {
+    handleLogin(userName, password);
+  };
+
+  return loginUser ? <Redirect to="/items" /> : (
     <div>
+      {message && message.map((msg) => (<p key={msg}>{msg}</p>))}
       <h2>Login</h2>
       <UserForm handleSubmit={handleSubmit} btnName="LogIn" />
     </div>
   );
 };
 
-export default Login;
+const mapStateToProps = (state) => ({
+  loginUser: state.user.logIn,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  signUp: (user) => dispatch(signUp(user)),
+  logIn: (status) => dispatch(logIn(status)),
+});
+
+Login.propTypes = {
+  history: PropTypes.instanceOf(Object),
+  logIn: PropTypes.bool.isRequired,
+  signUp: PropTypes.func,
+  loginUser: PropTypes.bool.isRequired,
+};
+
+Login.defaultProps = {
+  history: null,
+  signUp: {},
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
