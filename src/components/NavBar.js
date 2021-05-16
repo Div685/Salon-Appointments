7b1367a4addca798ae088c609dfc73766cfe8c68
Div685 <divyeshpatel685@gmail.com>
2021-05-16 22:59:46 +0300

    Add style to the NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,34 @@
 import React, { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import MenuIcon from '@material-ui/icons/Menu';
+import CancelIcon from '@material-ui/icons/Cancel';
 import { useSelector } from 'react-redux';
 import LogOutBtn from './LogOutBtn';
+import '../assets/css/NavBar.css';
 
 const NavBar = () => {
   const [toggleNavClass, setToggleNavClass] = useState('');
+  const [isClicked, setIsClicked] = useState(true);
 
   const isLoggedIn = useSelector((state) => state.user.logIn);
   const user = useSelector((state) => state.user);
 
+  const changeButtonName = () => {
+    setIsClicked(!isClicked);
+  };
+
   const handleToggle = () => {
     setToggleNavClass(toggleNavClass === ' active' ? '' : ' active');
+    changeButtonName();
   };
 
   const closeToggle = () => {
     setToggleNavClass('');
+    changeButtonName();
   };
 
   return (
-    <header className="d-flex justify-content-between p-2 align-items-center">
+    <header className="header d-flex justify-content-between p-2 align-items-center">
       <div className="nav__logo">
         <Link to="/">
           BookMySalon
@@ -27,10 +36,17 @@ const NavBar = () => {
       </div>
       <div className="nav__toggle">
         <button type="button" className={`header_button${toggleNavClass}`} onClick={handleToggle}>
-          <MenuIcon />
+          { isClicked ? (
+            <MenuIcon />
+          ) : (<CancelIcon />) }
         </button>
       </div>
       <nav className={`nav${toggleNavClass}`}>
+        <div className="nav__logo-tab">
+          <Link to="/">
+            BookMySalon
+          </Link>
+        </div>
         {isLoggedIn ? (
           <>
             <span>{`Welcome ${user.user.username}`}</span>
