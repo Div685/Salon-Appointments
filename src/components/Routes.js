07ce66bab75dfdfc5ec9077cfefd5539fce4e57e
Div685 <divyeshpatel685@gmail.com>
2021-05-16 20:16:28 +0300

    Add SignUp component and update Routes file

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-// import SalonItemList from '../container/SalonItemList';
+import PropTypes from 'prop-types';
+import SalonItemList from '../container/SalonItemList';
 import Login from '../container/Login';
 import SignUp from '../container/SignUp';
-import Home from './Home';
+import NavBar from './NavBar';
 
 const Routes = () => (
   <BrowserRouter>
+    <NavBar />
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/" component={SalonItemList} />
+      <Route exact path="/login" render={(props) => (<Login history={props.history} />)} />
+      <Route exact path="/signup" render={(props) => (<SignUp history={props.history} />)} />
+      <Route exact path="/items" component={SalonItemList} />
     </Switch>
   </BrowserRouter>
 );
+
+Routes.propTypes = {
+  history: PropTypes.instanceOf(Object),
+};
+
+Routes.defaultProps = {
+  history: null,
+};
 
 export default Routes;
--- src/container/SignUp.js
+++ src/container/SignUp.js
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signedUp } from '../api/authUserRequest';
 import UserForm from '../components/UserForm';
+import { signUp, logIn } from '../redux/actions';
 
-const SignUp = () => (
-  <div>
-    <h2>Sign up</h2>
-    <UserForm />
-  </div>
-);
+const SignUp = ({
+  signUp, logIn, history, loginUser,
+}) => {
+  const [message, setMessage] = useState([]);
 
-export default SignUp;
+  const handleSignUp = async (userName, password) => {
+    try {
+      const response = await signedUp(userName, password);
+      if (response.status === 'created') {
+        setMessage(['SuccessFully Created User!']);
+        localStorage.setItem('token', response.token);
+        signUp(response.user);
+        logIn(true);
+        history.push('/items');
+      } else {
+        setMessage(response.message);
+      }
+    } catch {
+      setMessage(['Sorry, Signup failed ']);
+    }
+  };
+
+  const handleSubmit = (userName, password) => {
+    handleSignUp(userName, password);
+  };
+
+  return loginUser ? <Redirect to="/items" /> : (
+    <div>
+      {message && <p className="info-msg">{message}</p>}
+      <h2>Sign up</h2>
+      <UserForm handleSubmit={handleSubmit} btnName="Sign Up" />
+      <Link to="/" className="btn">Home</Link>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  loginUser: state.user.logIn,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  signUp: (user) => dispatch(signUp(user)),
+  logIn: (status) => dispatch(logIn(status)),
+});
+
+SignUp.propTypes = {
+  history: PropTypes.instanceOf(Object),
+  logIn: PropTypes.bool,
+  signUp: PropTypes.func,
+  loginUser: PropTypes.bool.isRequired,
+};
+
+SignUp.defaultProps = {
+  history: null,
+  logIn: null,
+  signUp: {},
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
