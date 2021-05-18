50ae415cdd14f7be83a1831cabb8f06f8c8cbedb
Div685 <divyeshpatel685@gmail.com>
2021-05-19 01:04:27 +0300

    Add style to the SalonItems Component

--- src/container/SalonItemList.js
+++ src/container/SalonItemList.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { fetchItemList } from '../api/fetchItems';
 import Item from '../components/Item';
+import '../assets/css/SalonItems.css';
 
 const SalonItemList = () => {
   const items = useSelector((state) => state.items.items);
@@ -15,15 +16,19 @@ const SalonItemList = () => {
 
   return (
     <div className="items">
-      <h1>Welcome to Salon Item Lists</h1>
-      <p>Please select Items to Book an appointment with us.</p>
-      {
-        items && items.length
-          ? items.map((item) => (
-            <Item key={item.id} item={item} />
-          ))
-          : (heatMap())
-      }
+      <div className="items__header">
+        <h1>Our Services</h1>
+        <p>Please select service and Book an appointment with us.</p>
+      </div>
+      <div className="items__Lists">
+        {
+          items && items.length
+            ? items.map((item) => (
+              <Item key={item.id} item={item} />
+            ))
+            : (heatMap())
+        }
+      </div>
     </div>
   );
 };
