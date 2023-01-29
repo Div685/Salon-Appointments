bb902ae197f09f3b70f247d2a6887069b3460c0d
Div685 <divyeshpatel685@gmail.com>
2023-01-29 18:44:12 +0300

    Updated APIs url link

--- src/api/apiUrl.js
+++ src/api/apiUrl.js
@@ -1,3 +1,5 @@
-const apiUrl = 'https://salon-appointment-api.herokuapp.com/api/v1';
+const apiUrl = 'https://salonrailsapi.onrender.com/api/v1';
+// 'http://localhost:3001/api/v1';
+// 'https://salon-appointment-api.herokuapp.com/api/v1';
 
 export default apiUrl;
