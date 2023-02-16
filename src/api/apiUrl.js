97b340f4ac475c31708504bc4f0159c97e06e205
Div685 <divyeshpatel685@gmail.com>
2023-02-16 22:11:56 +0300

    Update api header content

--- src/api/apiUrl.js
+++ src/api/apiUrl.js
@@ -1,3 +1,4 @@
 const apiUrl = 'https://salonrailsapi.onrender.com/api/v1';
+// 'http://127.0.0.1:3001/api/v1';
 
 export default apiUrl;
--- src/api/request.js
+++ src/api/request.js
@@ -4,11 +4,13 @@ import apiUrl from './apiUrl';
 const authAxios = (token = null) => axios.create({
   baseURL: apiUrl,
   data: {},
+  mode: 'no-cors',
   headers: {
+    // 'Accept': 'application/json',
     'Content-Type': 'application/json',
     // 'Access-Control-Allow-Headers': 'X-CSRF-Token, Content-Type',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': '*',
+    // 'Access-Control-Allow-Origin': '*',
+    // 'Access-Control-Allow-Headers': '*',
     Authorization: `Bearer ${token}`,
   },
 });
